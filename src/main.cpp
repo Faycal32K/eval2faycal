ed146573c678479ba5ed2e9c31a9cd888e594958
faycal <faycalbouzidi4@gmail.com>
2023-12-15 13:59:32 +0000

    répétition du menu

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,7 @@ int main()  {
   int nbrePlace = 10;
   //variable permettant de récuperer le choix de l'utilisateur 
   int choix;
-  while (choix <1 || choix > 4 )
-    {
+  
   cout << "************ Evaluation 2 ************" << endl <<endl;
   // partie a compléter a partir du 3.1
 cout << "" << endl << endl;
@@ -30,6 +29,31 @@ cout << "" << endl << endl;
     cout << " Vous avez choisi l'option nummero 4";
       break;
   }
+  while (choix <1 || choix >4)
+    {
+        cout << "************ Evaluation 2 ************" << endl <<endl;
+  // partie a compléter a partir du 3.1
+cout << "" << endl << endl;
+  cout << "1 - Entrée dans le parking" << endl << endl;
+  cout << "2 - Nombre de place disponibles" << endl << endl;
+  cout << "3 - Sortie du parking" << endl << endl;
+  cout << "4 - Quitter" << endl << endl;
+  cin >> choix;
+  switch (choix) {
+    case 1:
+    cout << " Vous avez choisi l'option numero 1 ";
+      break;
+    case 2:
+    cout << " Vous avez choisi l'option numero 2";
+      break;
+    case 3 :
+    cout << " Vous avez choisi l'option numero 3";
+      break;
+      case 4:
+    cout << " Vous avez choisi l'option nummero 4";
+      break;
+    }
   return 0;
 }
 }
+
