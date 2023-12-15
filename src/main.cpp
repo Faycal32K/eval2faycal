d3925473dc3fabfee553e46674df4aeab27e741b
faycal <faycalbouzidi4@gmail.com>
2023-12-15 12:54:52 +0000

    ajout du menu

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,10 @@ int main()  {
   int choix;
   cout << "************ Evaluation 2 ************" << endl <<endl;
   // partie a compléter a partir du 3.1
-
-
+cout << "" << endl << endl;
+  cout << "1 - Entrée dans le parking" << endl << endl;
+  cout << "2 - Nombre de place disponibles" << endl << endl;
+  cout << "3 - Sortie du parking" << endl << endl;
+  cout << "4 - Quitter" << endl << endl;
   return 0;
 }
