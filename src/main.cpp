da69eabdd193a1c0d3eb7d7d35ccf5953cfd0382
faycal <faycalbouzidi4@gmail.com>
2023-12-15 14:50:59 +0000

    3.4

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main()  {
   //variable permettant de compter le nombre de places disponibles
-  int nbrePlace = 10;
+  int i = 10 ;
+  
   //variable permettant de récuperer le choix de l'utilisateur 
   int choix;
-  
+do{
   cout << "************ Evaluation 2 ************" << endl <<endl;
   // partie a compléter a partir du 3.1
 cout << "" << endl << endl;
@@ -17,43 +18,23 @@ cout << "" << endl << endl;
   cin >> choix;
   switch (choix) {
     case 1:
-    cout << " Vous avez choisi l'option numero 1 ";
+    cout << --i;
+      cout << " Place restantes";
       break;
     case 2:
-    cout << " Vous avez choisi l'option numero 2";
+      cout << i;
+    cout << " Place disponibles";
       break;
     case 3 :
-    cout << " Vous avez choisi l'option numero 3";
+      cout << ++i;
+    cout << " Place restantes";
       break;
       case 4:
-    cout << " Vous avez choisi l'option nummero 4";
+    cout << "Au-revoir";
       break;
   }
-  while (choix <1 || choix >4)
-    {
-        cout << "************ Evaluation 2 ************" << endl <<endl;
-  // partie a compléter a partir du 3.1
-cout << "" << endl << endl;
-  cout << "1 - Entrée dans le parking" << endl << endl;
-  cout << "2 - Nombre de place disponibles" << endl << endl;
-  cout << "3 - Sortie du parking" << endl << endl;
-  cout << "4 - Quitter" << endl << endl;
-  cin >> choix;
-  switch (choix) {
-    case 1:
-    cout << " Vous avez choisi l'option numero 1 ";
-      break;
-    case 2:
-    cout << " Vous avez choisi l'option numero 2";
-      break;
-    case 3 :
-    cout << " Vous avez choisi l'option numero 3";
-      break;
-      case 4:
-    cout << " Vous avez choisi l'option nummero 4";
-      break;
-    }
+}
+  while ( choix <4 || choix >4);
+
   return 0;
 }
-}
-
