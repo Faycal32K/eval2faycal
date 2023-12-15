e41d3083b99152eec35caaba67683f5d9ae053e9
faycal <faycalbouzidi4@gmail.com>
2023-12-15 12:31:53 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()  {
+  //variable permettant de compter le nombre de places disponibles
+  int nbrePlace = 10;
+  //variable permettant de récuperer le choix de l'utilisateur 
+  int choix;
+  cout << "************ Evaluation 2 ************" << endl <<endl;
+  // partie a compléter a partir du 3.1
+
+
+  return 0;
+}
